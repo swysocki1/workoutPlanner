9cbc503cc3e56450f6d75a834b281d48dbb208b4
kmiller-16 <kmille92@students.towson.edu>
2018-10-01 20:47:50 -0400

    commit 2

--- src/app/component/accountSignup/accountSignup.component.ts
+++ src/app/component/accountSignup/accountSignup.component.ts
@@ -1,9 +1,35 @@
 import {Component} from "@angular/core";
+import { User } from "../../../models/user.model";
+import {FormGroup} from "@angular/forms";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'account-signup',
   templateUrl: './accountSignup.html'
 })
 export class AccountSignupComponent {
+user = new User(); 
+  signUp: FormGroup = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+    username: new FormControl()
 
+  });
+    
+    submitted = false;
+
+    onSubmit() {this.submitted = true;
+      this.doSignUp();
+    }   
+
+doSignUp(){
+  this.user = new User();
+  this.user.firstName = this.signUp.value.firstName;
+  this.user.lastName = this.signUp.value.lastName;
+  this.user.username = this.signUp.value.username;
+  this.user.email = this.signUp.value.email;
+  this.user['password'] = '';
+  return this.user; 
+}
 }
