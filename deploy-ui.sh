d0c780309da2cbdcbc020566c788b6a644c35d82
Sean Wysocki <swysocki@C02WF3ZUHTD5.local>
2018-11-09 11:49:10 -0500

    fixed UI Build Script

--- deploy-ui.sh
+++ deploy-ui.sh
@@ -1,8 +1,8 @@
-npm run build
-
 # Refresh modules and remove existing zip file
 #rm -rf dist
 #mkdir dist
+
+npm run build
 
 export AWS_PROFILE=workoutPlanner
 aws s3 sync ./dist/ s3://workout-planner --acl public-read --delete
